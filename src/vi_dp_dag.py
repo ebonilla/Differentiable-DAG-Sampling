e0f93342dd9aafb566ca79e9cb5685e8b4892165
Edwin Bonilla <edwinbonilla@gmail.com>
2024-05-21 22:57:37 +1000

    command args for own data

--- src/vi_dp_dag.py
+++ src/vi_dp_dag.py
@@ -1,18 +1,16 @@
-from src.probabilistic_dag_model.probabilistic_dag_autoencoder import ProbabilisticDAGAutoencoder
-from src.probabilistic_dag_model.train_probabilistic_dag_autoencoder import train_autoencoder
-from src.datasets.DAGDataset import get_dag_dataset
+import argparse
 from src.run_probabilistic_dag_autoencoder import run
 
 param_dict = {
     'seed_dataset': 123,  # Seed to shuffle dataset. int
-    'dataset_name': 'data_p10_e10_n1000_GP',  # Dataset name. string
-    'dataset_directory': 'src/datasets/datasets_dp_dag',  # Dataset directory. string
+    'dataset_name': 'all',  # Dataset name. string
+    'dataset_directory': 'to_complete',  # Dataset directory. string
     'i_dataset': 1,  # Dataset name. string
     'split': [.8, .1, .1],  # Split for train/val/test sets. list of floats
 
     # Architecture parameters
     'seed_model': 123,  # Seed to init model. int
-    'directory_model': 'models_dp_dag',  # Path to save model. string
+    'directory_model': 'to_complete',  # Path to save model. string
     'ma_hidden_dims': [16, 16, 16],  # Output dimension. int
     'ma_architecture': 'linear',  # Output dimension. int
     'ma_fast': False,  # Output dimension. int
@@ -23,7 +21,7 @@ param_dict = {
     'pd_noise_factor': 1.0,  # Hidden dimensions. list of ints
 
     # Training parameters
-    'directory_results': 'results_dp_dag',  # Path to save resutls. string
+    'directory_results': 'to_complete',  # Path to save resutls. string
     'max_epochs': 100,  # Maximum number of epochs for training
     'patience': 10,  # Patience for early stopping. int
     'frequency': 2,  # Frequency for early stopping test. int
@@ -34,6 +32,16 @@ param_dict = {
     'regr': .01,  # Regularization factor in Bayesian loss. float
     'prior_p': .01  # Regularization factor in Bayesian loss. float
 }
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--data-dir", type=str, default="./", help="base results directory"
+)
+
+args = parser.parse_args()
+param_dict['dataset_directory'] = args.data_dir
+param_dict['directory_model'] = 'model_dds/' + args.data_dir
+param_dict['directory_results'] = 'results_dds' + args.data_dir
 
 results = run(**param_dict)
 
